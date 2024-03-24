added dots and fixed normalization!(?)

noodle.py
@@ -1,29 +1,94 @@
 import sigs
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import correlate
 
-BW = 50.0e6  # full bandwidth
-B = 0.1e6  # channel bandwidth
-tau = 2.0  # s
-oversample=10
-total_time = 2  # s
-Tsys = 50.0  # K
-Tsky = 100.0
-SNR = 0.000001
-freq = 23e6
+#https://www.dsprelated.com/showarticle/1004.php  calc power from DFT
+
+### INPUTS
+BW = 3.0e6  # full bandwidth
+oversample=1.0
+total_time = 0.4  # s
+Tsys = 30.0  # K
+Tsky = 0.0
+freq = 1.9e5  # Of signal
 phase = 0.0
+drift_rate = 0.0
+distance =10000.  # in LY
+EIRP = 1.0E12  # in W
+Ae = 50 * 50.
+SNR = 10.0
+integration_time = 18.0
+
+### NOISE
+sky = sigs.BandLimitedWhiteNoise(Tsky, BW, total_time, oversample)
+sky.band_limited_white_noise(integration_time=integration_time)
+sys1 = sigs.BandLimitedWhiteNoise(Tsys, BW, total_time, oversample)
+sys1.band_limited_white_noise(integration_time=integration_time)
+noise1 = sigs.CombinedSignal(sys1, sky)
+noise1.power_spectrum()
+noise1.integrate_v()
+noise1.integrate_f()
+
+## ET
+sig = sigs.DriftingCW(sys1.fs, total_time, starting_freq=freq, drift_rate=drift_rate, EIRP=EIRP, distance=distance)
+sig.cw(Ae, phase)
+sig.power_spectrum()
+sig.integrate_v()
+sig.integrate_f()
+
+## RECEIVED
+rx1 = sigs.CombinedSignal(sig, noise1)
+rx1.power_spectrum()
+rx1.integrate_v()
+rx1.integrate_f()
+
+print(noise1)
+print(f"Integration time = {integration_time}")
+print(f"Noise = {noise1.dB('channel_power')}  dB[W]")
+print(f"Signal = {sig.dB('channel_power')} dB[W]")
+print(f"SNR(threshhold) = {SNR}")
+print(f"SNR(detected) = {sig.channel_power/ noise1.channel_power}")
+print(f"Freq resolution = {sys1.fs / sys1.N} Hz")
+print(f"Band = {sys1.fs / 2.0} Hz")
+print("Integrating voltage^2 ...")
+print(f"  noise1 = {noise1.dB('Iv2') + sigs.to_dB(noise1.freq_resolution)}")
+print(f"  sig = {sig.dB('Iv2') + sigs.to_dB(sig.N)}")
+print(f"  rx1 = {rx1.dB('Iv2')}")
+print("Integrating power spectrum ...")
+print(f"  noise1 = {noise1.dB('If') + sigs.to_dB(noise1.freq_resolution)}")
+print(f"  sig = {sig.dB('If') + sigs.to_dB(sig.N)}")
+print(f"  rx1 = {rx1.dB('If')}")
+print("DIFF")
+print(noise1.dB('If') - noise1.dB('Iv2'))
+print(sig.dB('If') - sig.dB('Iv2'))
+
+### TIME PLOT
+plot_span = 500
+t_plot = sys1.t[:plot_span]
+figt, axt = plt.subplots()
+axt.plot(t_plot, noise1.signal[:plot_span], 'b')
+axt.plot(t_plot, sig.signal[:plot_span], 'g')
+axt.plot(t_plot, rx1.signal[:plot_span], 'r')
+axt.plot([t_plot[0], t_plot[-1]], [np.sqrt(noise1.channel_power), np.sqrt(noise1.channel_power)], 'k--')
+axt.plot([t_plot[0], t_plot[-1]], [np.sqrt(sig.W / 2.0), np.sqrt(sig.W / 2.0)], 'g--')
+
+### FREQUENCY PLOT
+figf, axf = plt.subplots()
+axf.plot(rx1.f, rx1.dB('S'), 'b')
+axf.plot([rx1.f[0], rx1.f[-1]], [noise1.dB('channel_power') + sigs.to_dB(SNR), noise1.dB('channel_power') + sigs.to_dB(SNR)])
+axf.plot([rx1.f[0], rx1.f[-1]], [sig.dB('channel_power'), sig.dB('channel_power')])
+axf.set_xlim(left=0, right=BW)
+axf.set_ylim(bottom=-220)
 
 
-A = sigs.SignalProc(Tsys, BW, B, tau, total_time, oversample=10.0, mu=0)
-s = A.signal(SNR, freq, phase)
-n_sky = A.band_limited_white_noise(Tsky)
+#a = correlate(x, x, mode='same')
+#print(type(a))
+#print(a.shape)
+#axf.plot(a)
 
-n_sys1 = A.band_limited_white_noise(Tsys)
-n_sys2 = A.band_limited_white_noise(Tsys)
+# u1 = sigs.float_to_uint8(x1)
+# u2 = sigs.float_to_uint8(x2)
 
-x1 = s + n_sky + n_sys1
-x2 = s + n_sky + n_sys2
-
-u1 = sigs.float_to_uint8(x1)
-u2 = sigs.float_to_uint8(x2)
-
-sigs.write_numpy_array_to_binary(u1, 'obs1.bin')
-sigs.write_numpy_array_to_binary(u2, 'obs2.bin')
+# sigs.write_numpy_array_to_binary(u1, 'obs1.bin')
+# sigs.write_numpy_array_to_binary(u2, 'obs2.bin')

sigs.py
@@ -1,63 +1,101 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+rng = np.random.default_rng()
 
 
-kB = 1.380649e-23
+kB = 1.380649e-23  # J/K
+LY = 9.461e+15  # m/ly
 
 
-class SignalProc:
+def to_dB(v):
+    return 10.0 * np.log10(v)
 
-    def __init__(self, Tsys=None, BW=None, B=None, tau=None, total_time=None, oversample=10.0, mu=0):
+class Signal:
+    def __repr__(self):
+        return f"T={self.T}\nBW={self.BW}\nfs={self.fs}\nN={self.N}"
+
+    def power_spectrum(self):
+        f, sv = fft(self.fs, self.signal)
+        S = 2.0 * (np.abs(sv))**2 / self.N
+        pn = len(f) // 2
+        self.f = f[:pn]
+        self.S = S[:pn]
+
+    def dB(self, a):
+        v = getattr(self, a)
+        return to_dB(v)
+
+    def delay(self, t_delay=0.0):
+        """
+        Parameter
+        ---------
+        t_delay : float
+            start delay in microseconds
+        """
+        print("Delay the signal.")
+
+    def integrate_v(self):
+        self.Iv2 =  np.trapz(self.signal**2, self.t) / self.total_time
+        
+    def integrate_f(self):
+        self.If =  np.trapz(self.S, self.f) * self.total_time / self.N
+
+class CombinedSignal(Signal):
+    def __init__(self, signal1, signal2):
+        self.signal = signal1.signal + signal2.signal
+        self.channel_power = signal1.channel_power + signal2.channel_power
+        self.fs = signal1.fs
+        self.N = signal1.N
+        self.t = signal1.t
+        self.freq_resolution = signal1.freq_resolution
+        self.t = signal1.t
+        self.BW = signal1.BW
+        self.total_time = signal1.total_time
+        self.T = 0.0
+        try:
+            self.T += signal1.T
+        except AttributeError:
+            pass
+        try:
+            self.T += signal2.T
+        except AttributeError:
+            pass
+
+class BandLimitedWhiteNoise(Signal):
+    def __init__(self, T=50.0, BW=50.0e6, total_time=1.0, oversample=2.0):
         """
         Parameters
         ----------
-        Tsys : float
+        T : float
             System temperature in K
-        B : float
-            Channel bandwidth in Hz
-        tau : float
-            Integration time in sec
-        sr : float
-            Sample rate in Hz
-        N : int
-            Number of samples
-        mu : float
-            Mean
+        BW : float
+            Bandwidth in Hz
+        total_time : float
+            Total time to "record" voltages in s
+        oversample : float
+            Percentage to oversample (primarily to see the roll-off)
 
         """
-        self.Tsys = Tsys
+        self.T = T
         self.BW = BW
         self.total_time = total_time
-        self.mu = mu
-        self.B = B
-        self.tau = tau
-        if self.BW is not None:
-            self.fs = np.floor(self.BW * 2.0 * (1.0 + oversample/100.0))  # /s
-            self.N = N = int(self.total_time * self.fs)
-            print(f"Tsys={Tsys}\nBW={BW}\nB={B}\ntau={tau}\nsr={self.fs}\nN={N}")
-
-    def signal(self, SNR, freq, phase=0.0):
-        self.freq = freq
-        self.phase = phase
-        self.t = np.linspace(0, self.total_time, self.N)
-        return np.sqrt(SNR * kB * self.Tsys * self.B) *  np.sin(2.0 * np.pi * freq * self.t + phase)
+        self.mu = 0.0  # Could be a bias someday...?
+        self.fs = np.floor(self.BW * 2.0 * (1.0 + oversample/100.0))  # /s
+        self.N = int(self.total_time * self.fs)
+        self.freq_resolution = self.fs / self.N
+        self.t = np.linspace(0.0, self.total_time, self.N)
+        self.channel_power = kB * self.T * self.freq_resolution
+        self.total_power = kB * self.T * self.BW
 
     # AWGN https://stackoverflow.com/questions/14058340/adding-noise-to-a-signal-in-python
-    def band_limited_white_noise(self, T):
-        # f = np.fft.fftfreq(N, 1 / sr)
-        rho2 = kB * T
-        sigma = np.sqrt(rho2) # * sr / 2.0)
-        return self.butter_lowpass_filter(np.random.normal(self.mu, sigma, self.N), order=8)
-        # plt.figure('time')
-        # plt.plot(noise)
-        # plt.plot(lpnoise)
-        # plt.figure('Freq')
-        # plt.plot(np.fft.fft(noise).real)
-        # plt.plot(np.fft.fft(lpnoise).real)
+    def band_limited_white_noise(self):
+        var = kB * self.T
+        sigma = np.sqrt(var)
+        self.signal = self.butter_lowpass_filter(rng.normal(self.mu, sigma, self.N), order=4)
 
     def butter_lowpass_filter(self, data, order=8):
         from scipy.signal import butter, filtfilt, freqz
-        # print("Cutoff freq " + str(cutoff))
         nyq = 0.5 * self.fs # Nyquist Frequency
         normal_cutoff = self.BW / nyq
         # Get the filter coefficients 
@@ -67,25 +105,69 @@ class SignalProc:
         y = filtfilt(b, a, data)
         return y
 
-    def fft(self, data):
-        return np.fft.fft(data).real
 
-    def Smin(self):
+class DriftingCW(Signal):
+    def __init__(self, fs, total_time, starting_freq, drift_rate=0.0, EIRP=1.0E12, distance=10.0):
         """
-        This just implements Eq 1/2 in https://iopscience.iop.org/article/10.3847/1538-3881/acfc1e/pdf
-        for canonical case.
+        Parameters
+        ----------
+        fs : float
+            sample rate in Hz
+        total_time : float
+            total_time of recording in s
+        starting_freq : float
+            starting frequency in Hz
+        drift_rate : float
+            linear drift rate in Hz/s
+        EIRP : float
+            effective isotropic radiated power in W
+        distance : float
+            distance to signal in ly
 
         """
-        SNR = 5.0  # Factor of 5 in SNR
-        dnu = 1.0  # 1 Hz
-        tobs = 5.0 * 60.0  # Five minutes
-        npol = 2.0
-        Tsys = 25.0
-        Ae = 0.85 * np.pi * 50.0 * 50.0  # GBT
-        Smin = SNR * 2.0 * kB / (Ae / Tsys) * np.sqrt(dnu / (npol*tobs))
-        d = 4.2 * 9.461e+15  # m to nearest star
-        EIRPmin = 4.0 * np.pi * d * d * Smin
-        print(Smin, EIRPmin / 1e9)
+        self.fs = fs
+        self.total_time = total_time
+        self.N = int(self.total_time * self.fs)
+        self.freq_resolution = self.fs / self.N
+        self.BW = self.fs / 2.0
+        self.starting_freq = starting_freq
+        self.drift_rate = drift_rate
+        self.EIRP = EIRP
+        self.distance = distance * LY
+        self.Wm2 = self.EIRP / (4.0 * np.pi * self.distance**2)
+        self.t = np.linspace(0, self.total_time, self.N)
+
+    def cw(self, Ae=1.0, phase=0.0):
+        self.A = Ae
+        self.W = self.Wm2 * Ae
+        self.phase = phase
+        Vm = np.sqrt(self.W)
+        self.freq = np.linspace(self.starting_freq, self.starting_freq + self.total_time * self.drift_rate, self.N)
+        self.signal = Vm *  np.sin(2.0 * np.pi * self.freq * self.t + phase)
+        self.channel_power = self.W * self.N / 2.0
+
+
+def fft(fs, data):
+    f = np.fft.fftfreq(len(data), 1 / fs)
+    sigf = np.fft.fft(data)
+    return f, sigf
+
+def Smin(self):
+    """
+    This just implements Eq 1/2 in https://iopscience.iop.org/article/10.3847/1538-3881/acfc1e/pdf
+    for canonical case.
+
+    """
+    SNR = 5.0  # Factor of 5 in SNR
+    dnu = 1.0  # 1 Hz
+    tobs = 5.0 * 60.0  # Five minutes
+    npol = 2.0
+    T = 25.0
+    Ae = 0.85 * np.pi * 50.0 * 50.0  # GBT
+    Smin = SNR * 2.0 * kB / (Ae / T) * np.sqrt(dnu / (npol*tobs))
+    d = 4.2 * 9.461e+15  # m to nearest star
+    EIRPmin = 4.0 * np.pi * d * d * Smin
+    print(Smin, EIRPmin / 1e9)
 
 import struct
 def float_to_uint8(array):